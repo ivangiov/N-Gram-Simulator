4bb20adc92008ea4e68100882c2143c36dd14c41
sarahalyaat <alya.sarah.rara@gmail.com>
2020-04-12 21:04:17 +0700

    Update searchVal.c

--- searchVal.c
+++ searchVal.c
@@ -17,11 +17,12 @@ node* randomHead(node **text,int n){
 	int r = (rand() % n) + 1;
 	int i = 0;
 	node *head = *text;
-	printf("%d\n",r);
 
+	//Mencari node ke-r
 	for(i = 0; i < r; i++){
 		head = head->next;
 	}
+
 	return (head);
 }
 
@@ -31,15 +32,23 @@ void searchValue(node **text, queue **key, int n){
 	node *testText;
 	node *currKey;
 	int found = 0;
+
 	while (found == 0){
 		currKey = (*key)->front;
 		testText = currText;
+
 		while(currKey!=NULL){
+
+			//Kasus kata tidak sama
 			if (strcmp(testText->word,currKey->word)!=0)
 				break;
+
+			//Kasus kata sama
 			currKey = currKey->next;
 			testText = testText->next;
 		}
+
+		//Jika key ditemukan pada text
 		if (currKey==NULL){
 			found = 1;
 		}
@@ -47,8 +56,11 @@ void searchValue(node **text, queue **key, int n){
 			currText = currText->next;
 		}
 	}
+
+	//Print the value
 	printf("%s ",testText->word);
-	// dequeue(key);
-	// enqueue(key, testText->word);
-	
+
+	//Editing the key linked list
+	dequeue(key);
+	enqueue(key, testText->word);
 }
