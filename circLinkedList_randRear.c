cca3fd793972f183c42d07dace8075fabe80857d
Christiand6 <34437533+Christiand6@users.noreply.github.com>
2020-04-13 16:37:48 +0700

    Minor update on randRear

--- circLinkedList_randRear.c
+++ circLinkedList_randRear.c
@@ -14,10 +14,10 @@ node *randRear (node *, int);
 
 /* fungsi menambahkan node pada ujung akhir list */
 node *AddNode (char *kata, node *rear){
-    // deklarasi node baru
-    node *temp = (node*)malloc(sizeof(node));
-    temp->word = (char*)malloc(sizeof(char));
-    strcpy(temp->word,kata);
+   
+    node *temp = (node*)malloc(sizeof(node));   // deklarasi node baru
+    temp->word = (char*)malloc(sizeof(char));   // alokasikan memori untuk word
+    strcpy(temp->word,kata);                    // isi word dengan kata
 
     /* untuk menangani list kosong */
     if (rear == NULL){
@@ -27,7 +27,7 @@ node *AddNode (char *kata, node *rear){
     }
     
     /* untuk menangani list yang tidak kosong */
-    strcpy(temp->word,kata);    // isi word dengan kata
+        
     temp->next = rear->next;    // tambahkan node di ujung akhir list dan circular
     rear->next = temp;          // pastikan temp berada pada ujung akhir dengan pointer rear sebelumnya mengarah ke temp
     rear = temp;                // deklarasi rear baru pada temp
@@ -37,18 +37,18 @@ node *AddNode (char *kata, node *rear){
 
 /* fungsi untuk mendapatkan posisi rear secara acak dari list */
 node *randRear (node *rear, int total){
+    srand(time(0));
     int rand_int;
     int i;
-    rand_int = (rand() % (total - 0 + 1)); // generate random integer from 0 to total
+    rand_int = (rand() % (total)); // generate random integer from 0 to total
     i = rand_int;
-    rear = rear->next;
+    rear = rear->next;             // move rear to the first node of list (usually head)
 
     /* randomize new position of rear */
     while (i != 0){ 
         rear = rear->next;
         i--;
     }
-    
+
     return rear;
 }
-
