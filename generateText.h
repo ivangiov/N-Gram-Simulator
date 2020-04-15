cb65b880c71ac04078c8d171151e40fe781393af
sarahalyaat <alya.sarah.rara@gmail.com>
2020-04-15 17:38:46 +0700

    Update generateText.h

--- generateText.h
+++ generateText.h
@@ -1,3 +1,12 @@
+//Fungsi Free List
+/* Mengosongkan isi linked list
+yang sudah tidak diakses*/
 void freeList(node** head, int n);
+
+//Fungsi Search Value
+/* Mencari value dari key pada linked list*/
 void searchValue(node **text, queue **key, int textLength);
+
+//Fungsi Generate Text
+/* Mencetak string text baru */
 void generateText(node*text, int textLength, int word_count, int n_grams);
