4a4d33956693058727b6b44d5074e3f4df649ce8
ivangiov <44900351+ivangiov@users.noreply.github.com>
2020-04-15 15:18:08 +0700

    Update function.h

--- function.h
+++ function.h
@@ -268,60 +268,13 @@ int menux(){
 	return(output);
 	}
 
-int main(){
-	srand(time(0));
+void freeList(node **head, int n){
+	node *temp;
+	int i;
 	
-	node* linkedList = (node*)malloc(sizeof(char));
-	linkedList = NULL;
-     
-	int textLength = 0;
-	int n_gram;
-	int random;
-	int menu;
-     
-	aboutUs();
-	
-	printf("SELAMAT DATANG DI N-PROGRAM\n");
-	printf("Program memiiliki tiga menu, yaitu input text eksternal, cetak kata-kata random dari n-gram, dan exit\n");
-
-	menu = menux();
-	while(menu != 3){
-		if (menu == 1){//input file eksternal
-			printf("Masukkan nama file : ");
-			char* filename = (char*)malloc(sizeof(char));
-			scanf("%s", filename);
-			textLength = fileHandling(filename, &linkedList);
-			free(filename);
-			printf("%d\n",textLength);
-			menu = menux();
-			}
-		else if(menu == 2){//mencetak kata-kata dari key n_gram
-			if(textLength == 0){
-				printf("File eksternal belum diinput, silakan input terlebih dahulu!\n");
-				menu = 1;
-				}
-			else{
-				printf("Masukkan banyaknya n pada n-gram: ");
-				scanf("%d", &n_gram);
-				while(n_gram < 2){
-					printf("n harus bernilai sama dengan dua atau lebih\n");
-					scanf("%d", &n_gram);}
-				printf("Masukkan jumlah kata random yang akan dicetak: ");
-				scanf("%d", &random);
-				generateText(linkedList, textLength, random, n_gram);
-				printf("\nyay!");
-				menu = menux();
-				}
-			}
-		else if(menu == 3){//keluar
-			printf("Terima kasih telah menggunakan program kami!\n");
-			free(linkedList);
-			exit(1);
-			}
-		else{//pilihan salah
-			printf("Masukkan input salah! Silakan input kembali");
-			menu = menux();
-			}
-		}
-	return(0);
- }
+	for (i = 0; i<n; ++i){
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
