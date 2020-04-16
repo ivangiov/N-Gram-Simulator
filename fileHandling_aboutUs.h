f1a0c4a49272fe8e9b591d43a3319fa262fece72
ivangiov <44900351+ivangiov@users.noreply.github.com>
2020-04-16 15:08:07 +0700

    Create fileHandling_aboutUs.h

---
+++ fileHandling_aboutUs.h
@@ -0,0 +1,7 @@
+//Fungsi fileHandling
+/* Fungsi untuk memuat data dari file txt ke circular linked list dan menghitung total jumlah kata */
+int fileHandling(char* filename,node **rear);
+
+//Prosedur aboutUs
+/* Prosedur untuk menampilkan judul program dan credits */
+void aboutUs();
