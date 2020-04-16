84d5b07420cf024376227b0aa7f01bd6d608069e
Dhanurangga Al Fadh <rangga.dhanu10@gmail.com>
2020-04-16 15:27:47 +0700

    Create queuehandling.c

---
+++ queuehandling.c
@@ -0,0 +1,67 @@
+/*
+Programmer : Dhanurangga Al Fadh (13218005)
+Tester : Sarah Alyaa Tsaabitah (13218011)
+Deskripsi : Berisi implementasi fungsi untuk mengolah queue. Terdapat dua fungsi yaitu enqueue untuk menambah data dan dequeue untuk mengeluarkan data
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+	char *word;
+	struct node *next;
+} node;
+
+typedef struct queue{
+	node *front, *rear;
+} queue;
+
+void enqueue(queue *Q, char *text){
+    //Membuat node baru dan meletakkan datanya
+    node *newnode = (node*)malloc(sizeof(node));
+    newnode->word = (char*)malloc(sizeof(char));
+    strcpy(newnode->word,text);
+
+    //Kasus queue masih kosong
+    if(Q->rear == NULL){
+        newnode->next = newnode;
+        Q->rear= newnode;
+        Q->front= newnode;
+        return;
+    }
+
+    //Kasus queue sudah terisi
+    Q->rear->next = newnode;
+    newnode->next = NULL;
+    Q->rear = newnode;
+    return;
+}
+
+
+void dequeue(queue *Q){
+    //Membuat pointer node sementara;
+    node *temp;
+
+    //Kasus queue kosong
+    if(Q->front == NULL){
+        printf("Queue kosong!\n");
+        return;
+    }
+
+    //Kasus tersisa node terakhir
+    //kasus ini gatau kenapa gabisa
+    else if(Q->front == Q->rear){
+        temp = Q->front;
+        Q->front = NULL;
+        Q->rear = NULL;
+        free(temp);
+        return;
+    }
+
+    //Kasus terdapat lebih dari 1 node
+    temp = Q->front;
+    Q->front = Q->front->next;
+    free(temp);
+    return;
+}
