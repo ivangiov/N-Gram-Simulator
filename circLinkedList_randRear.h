e61be1c6fabcdcc5770fc70a1b43a53d15115b94
Christiand6 <34437533+Christiand6@users.noreply.github.com>
2020-04-16 15:17:21 +0700

    Update circLinkedList_randRear.h

--- circLinkedList_randRear.h
+++ circLinkedList_randRear.h
@@ -1,6 +1,6 @@
 //Fungsi AddNode
 /* Generate Circular Linked List yang menyimpan kata per kata hasil parsing pada tiap node */
-node *AddNode (char *kata, node *rear);
+void *AddNode (char *kata, node *rear);
 
 //Fungsi randRear
 /* Mengacak posisi rear dari Circular Linked List yang kemudian dijadikan head pada list */
