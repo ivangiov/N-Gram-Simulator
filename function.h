ad8220fdac87f22ae2ae2f53ecab5261764dd7c6
sarahalyaat <47882708+sarahalyaat@users.noreply.github.com>
2020-04-17 19:38:41 +0700

    Update function.h

--- function.h
+++ function.h
@@ -1,32 +1,5 @@
-#include "integrating.h"
-#include "generateText.h"
-#include "queuehandling.h"
-#include "fileHandling_aboutUs.h"
-#include "circLinkedList_randRear.h"
-
-//Prosedur untuk memunculkan loading bar
-void loading(void);
-void aboutUs();
-void enqueue(queue *Q, char *text);
-void dequeue(queue *Q);
-
-// fungsi menambahkan node pada ujung akhir list
-void addNode (char *kata, node **rear);
-int fileHandling(char* filename, node **rear);
-
-// fungsi untuk mendapatkan posisi rear secara acak dari list
-node *randRear (node *rear, int total);
-
-//Fungsi ngerandom node awal di text
-node* randomHead(node **text,int textLength);
-
-//Fungsi mencari value dari key pada text
-void searchValue(node **text, queue **key, int textLength);
-
-//Fungsi membuat text baru
-void generateText(node *text, int textLength, int word_count, int n_grams);
-
-//Fungsi untuk menampilkan menu
-int menux();
-
-void freeList(node **head, int n);
+// #include "integrating.h"
+// #include "generateText.h"
+// #include "queuehandling.h"
+// #include "fileHandling_aboutUs.h"
+// #include "circLinkedList_randRear.h"
