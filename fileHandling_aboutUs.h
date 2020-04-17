e6d27c2ea87b1f433f2b8c6b689f596f7719e0ac
zulfikararifuzzaki <60221374+zulfikararifuzzaki@users.noreply.github.com>
2020-04-17 09:21:26 +0700

    Update fileHandling_aboutUs.h

--- fileHandling_aboutUs.h
+++ fileHandling_aboutUs.h
@@ -1,3 +1,8 @@
+#include "fileHandling_aboutUs.c"
+
+#ifndef FILEHANDLING_ABOUTUS_H
+#define FILEHANDLING_ABOUTUS_H
+
 //Fungsi fileHandling
 /* Fungsi untuk memuat data dari file txt ke circular linked list dan menghitung total jumlah kata */
 int fileHandling(char* filename,node **rear);
@@ -5,3 +10,5 @@ int fileHandling(char* filename,node **rear);
 //Prosedur aboutUs
 /* Prosedur untuk menampilkan judul program dan credits */
 void aboutUs();
+
+#endif
