cc8c20674fd79fffc6c8dda8a5f7892086e0e907
zulfikararifuzzaki <60221374+zulfikararifuzzaki@users.noreply.github.com>
2020-04-17 09:10:04 +0700

    Update circLinkedList_randRear.h

--- circLinkedList_randRear.h
+++ circLinkedList_randRear.h
@@ -1,3 +1,8 @@
+#include "circLinkedList_randRear.c"
+
+#ifndef CIRCLINKEDLIST_RANDREAR_H
+#define CIRCLINKEDLIST_RANDREAR_H
+
 //Fungsi AddNode
 /* Generate Circular Linked List yang menyimpan kata per kata hasil parsing pada tiap node */
 void AddNode (char *kata, node **rear);
@@ -5,3 +10,5 @@ void AddNode (char *kata, node **rear);
 //Fungsi randRear
 /* Mengacak posisi rear dari Circular Linked List yang kemudian dijadikan head pada list */
 node *randRear (node *rear, int total);
+
+#endif
